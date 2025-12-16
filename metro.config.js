5e28e80af31ea60965f8492af0adf5d34e5f0142
damiank1106 <damiank1106@yahoo.com>
2025-12-16 13:10:14 -0600

    Fix zod/v4 resolution

--- metro.config.js
+++ metro.config.js
@@ -3,5 +3,8 @@ const { withRorkMetro } = require('@rork-ai/toolkit-sdk/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// Needed for some subpath/exports-based imports like "zod/v4"
+config.resolver.unstable_enablePackageExports = true;
+
 module.exports = withRorkMetro(config);
 
